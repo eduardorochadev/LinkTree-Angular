4b260f9394559758f60d859b08bcff1fa47fc82f
eduardorochadev <chicoedumacedo@gmail.com>
2024-08-28 10:59:08 -0300

    commit-initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LinktreeComponent } from './linktree/linktree.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LinktreeComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/linktree/linktree.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-linktree',
+  standalone: true,
+  imports: [],
+  templateUrl: './linktree.component.html',
+  styleUrl: './linktree.component.css'
+})
+export class LinktreeComponent {
+
+}

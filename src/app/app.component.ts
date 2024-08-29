60363e7be85e6f5d6f80233051a5500ee40d91a4
eduardorochadev <chicoedumacedo@gmail.com>
2024-08-29 12:01:09 -0300

    criação de rotas, estilização da pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LinktreeComponent } from './linktree/linktree.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LinktreeComponent, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, LinktreeComponent, FooterComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { LinktreeComponent } from './linktree/linktree.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/linktree', pathMatch: 'full' }, // Redireciona a rota vazia para o componente LinktreeComponent
+  { path: 'linktree', component: LinktreeComponent }, // Componente LinktreeComponent
+  { path: 'about-me', component: AboutMeComponent }, // Componente AboutMeComponent
+  { path: '**', redirectTo: '/linktree' } // Redireciona qualquer rota não encontrada para o LinktreeComponent
+];
+
